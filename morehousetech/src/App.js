fd5de3a48ce16e35a873996d499056841b1b87a6
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-10-05 01:49:33 -0400

    Clean up for new branch

--- morehousetech/src/App.js
+++ morehousetech/src/App.js
@@ -7,31 +7,8 @@ class App extends Component {
         return (
           <Screen style={{alignItems: 'center', justifyContent: 'center'}}>
             <Title>Ibrahim</Title>
-            <Divider />
-            <Title>Jorja Smith</Title>
-            <Divider />
           </Screen>
         );
     }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-
 export default App;

fb853e77766e55cdfa00bd183ddda0154b124f9e
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-10-05 02:16:16 -0400

    Add the users reducers

---
+++ morehousetech/src/reducers/user.js
@@ -0,0 +1,11 @@
+/**
+* Reducers for set/get users.
+*/
+const defaultUser = {
+  email: null,
+  name: null,
+  uid: null,
+  avatar: https:'abs.twimg.com/sticky/default_profile_images/default_profile_3_400x400.png',
+  authorizing: false,
+  autorized: false
+}

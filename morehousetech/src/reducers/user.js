e899e249ad428d1b572d51ef3bbab7d1f859119a
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-10-07 03:13:18 -0400

    Add the first user reducer

---
+++ morehousetech/src/reducers/user.js
@@ -0,0 +1,12 @@
+/**
+* The getters and setters for the user reducer.
+*/
+
+const initialState = {
+    email: null,
+    name: null,
+    uid: null,
+    avatar: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_3_400x400.png',
+    authorizing: false,
+    authorized: false
+};

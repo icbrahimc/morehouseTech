cbaac3f265f38e26152212bc5dc07ed77f0e1e33
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-10-07 03:16:38 -0400

    Add the first UI compnents for the authentication page

---
+++ morehousetech/src/components/AuthenticationUI.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ReactNative from 'react-native';
+import { Screen, Title, Text, Divider, Button, Spinner } from '@shoutem/ui';
+
+class AuthenticationUI extends Component {
+  constructor(props) {
+    super.constructor(props);
+  }
+}
